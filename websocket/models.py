ad0796a01d8ddbc47e9aa3b2ecf35b2698b3b695
SAMUEL OYEWOLE <meetsamueloyewole@gmail.com>
2021-06-08 00:25:06 +0100

    AWS API + Websocket Chat Application

--- websocket/models.py
+++ websocket/models.py
@@ -4,8 +4,15 @@ from django.db import models
 
 class Connection(models.Model):
     connection_id = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.connection_id
+    
 
 class ChatMessage(models.Model):
     username = models.CharField(max_length=50)
     messages = models.CharField(max_length=400)
     timestamp = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.username
